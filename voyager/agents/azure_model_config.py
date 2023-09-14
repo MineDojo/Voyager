8d3a1db83bc50869895af3e54942622156f70bdb
Steve Wang <steve235lab@hotmail.com>
2023-09-14 14:34:13 +0800

    Supporting OpenAI Azure deployments.

--- voyager/agents/azure_model_config.py
+++ voyager/agents/azure_model_config.py
@@ -3,8 +3,8 @@ from pydantic import BaseModel
 
 class AzureModelConfig(BaseModel):
     """AzureChatOpenAI config profile"""
-    openai_api_base: str
-    openai_api_version: str
-    deployment_name: str
-    openai_api_type: str = "azure"
-    openai_api_key: str
+    openai_api_base: str = ''
+    openai_api_version: str = ''
+    deployment_name: str = ''
+    openai_api_type: str = 'azure'
+    openai_api_key: str = ''
